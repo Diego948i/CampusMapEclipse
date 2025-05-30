79e57ea180c133c94efc081a47c4456181ca8abc
DidiPme <diegoandre3b@gmail.com>
2025-05-29 22:33:48 -0300

    Initial commit

---
+++ src/campus/objects/MidRect.java
@@ -0,0 +1,23 @@
+package campus.objects;
+
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class MidRect {
+	private Rectangle midRect;
+	private Text midText;
+	
+	public Text getMidText() {
+		return midText;
+	}
+	public void setMidText(Text midText) {
+		this.midText = midText;
+	}
+	public Rectangle getMidRect() {
+		return midRect;
+	}
+	public void setMidRect(Rectangle midRect) {
+		this.midRect = midRect;
+	}
+	
+}
